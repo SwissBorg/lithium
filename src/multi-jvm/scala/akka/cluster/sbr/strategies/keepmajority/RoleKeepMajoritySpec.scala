8b7566d0c26b8c6739b1a3aa163c8d030e07614b
Dennis van der Bij <dennis@swissborg.com>
2019-03-19 10:43:51 +0100

    add keep-majority test with role

---
+++ src/multi-jvm/scala/akka/cluster/sbr/strategies/keepmajority/RoleKeepMajoritySpec.scala
@@ -0,0 +1,11 @@
+package akka.cluster.sbr.strategies.keepmajority
+
+import akka.cluster.sbr.FiveNodeSpec
+
+class RoleKeepMajoritySpecMultiJvmNode1 extends RoleKeepMajoritySpec
+class RoleKeepMajoritySpecMultiJvmNode2 extends RoleKeepMajoritySpec
+class RoleKeepMajoritySpecMultiJvmNode3 extends RoleKeepMajoritySpec
+class RoleKeepMajoritySpecMultiJvmNode4 extends RoleKeepMajoritySpec
+class RoleKeepMajoritySpecMultiJvmNode5 extends RoleKeepMajoritySpec
+
+class RoleKeepMajoritySpec extends FiveNodeSpec(RoleKeepMajoritySpecConfig)
