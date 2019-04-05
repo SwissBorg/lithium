46553b55922484239701b6687a2a045032c6f3c0
Dennis van der Bij <dennis@swissborg.com>
2019-04-05 17:09:30 +0200

    add ReachableNode test

---
+++ src/test/scala/akka/cluster/sbr/ReachableNodeSpec.scala
@@ -0,0 +1,14 @@
+package akka.cluster.sbr
+
+import akka.cluster.sbr.ArbitraryInstances._
+import akka.cluster.sbr.implicits._
+
+class ReachableNodeSpec extends MySpec {
+  "ReachableNode" - {
+    "1 - should not affect the order" in {
+      forAll { reachableNodes: List[ReachableNode] =>
+        reachableNodes.sorted.map(_.member) should ===(reachableNodes.map(_.member).sorted)
+      }
+    }
+  }
+}
