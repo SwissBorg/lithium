79539e744f2532fcef2ac4d00e25feee3f70ab2d
Dennis van der Bij <dennis@swissborg.com>
2019-05-06 10:48:50 +0200

    purify StabilityReporter

--- src/main/scala/akka/cluster/sbr/StabilityReporter.scala
+++ src/main/scala/akka/cluster/sbr/StabilityReporter.scala
@@ -1,8 +1,11 @@
 package akka.cluster.sbr
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Cancellable, Props, Stash}
+import akka.actor.{Actor, ActorLogging, ActorRef, Address, Props, Stash, Timers}
 import akka.cluster.ClusterEvent._
 import akka.cluster.{Cluster, Member}
+import cats.data.{State, StateT}
+import cats.effect.SyncIO
+import cats.implicits._
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.FiniteDuration
@@ -13,76 +16,77 @@ class StabilityReporter(downer: ActorRef,
                         cluster: Cluster)
     extends Actor
     with Stash
-    with ActorLogging {
+    with ActorLogging
+    with Timers {
   import StabilityReporter._
 
   private val selfMember: Member = cluster.selfMember
 
   private val _ = context.system.actorOf(SBRFailureDetector.props(self), "sbr-fd")
 
-  private var _handleSplitBrain: Cancellable = scheduleHandleSplitBrain()
-
-  private var _state: StabilityReporterState = StabilityReporterState(selfMember)
-
   override def receive: Receive = initializing
 
   private def initializing: Receive = {
     case s: CurrentClusterState =>
       unstashAll()
-      _state = StabilityReporterState.fromSnapshot(s, selfMember)
-      context.become(active)
+      context.become(active(StabilityReporterState.fromSnapshot(s, selfMember)))
 
     case _ => stash()
   }
 
-  private def active: Receive = {
-    case e: MemberEvent =>
-      resetHandleSplitBrain()
-      _state = _state.enqueue(e)
-
-    case SeenChanged(convergence, seenBy) =>
-      _state = _state.flush(seenBy)
-
-    case e: ReachabilityEvent =>
-      log.debug("{}", e)
-      resetHandleSplitBrain()
-
-      e match {
-        case UnreachableMember(member) => _state = _state.unreachableMember(member)
-        case ReachableMember(member)   => _state = _state.reachableMember(member)
-      }
-
-    case i @ IndirectlyConnectedMember(member) =>
-      log.debug("{}", i)
-      resetHandleSplitBrain()
-      _state = _state.indirectlyConnected(member)
-
+  private def active(state: StabilityReporterState): Receive = {
+    case e: MemberEvent                   => context.become(active(memberEvent(e).runS(state).unsafeRunSync()))
+    case SeenChanged(convergence, seenBy) => context.become(active(seenChanged(seenBy).runS(state).value))
+    case ReachableMember(m)               => context.become(active(reachableMember(m).runS(state).unsafeRunSync()))
+    case UnreachableMember(m)             => context.become(active(unreachableMember(m).runS(state).unsafeRunSync()))
+    case IndirectlyConnectedMember(m)     => context.become(active(indirectlyConnected(m).runS(state).unsafeRunSync()))
     case HandleSplitBrain =>
-      //        cancelClusterIsUnstable()
-      log.debug("Handle split brain")
-      downer ! Downer.HandleSplitBrain(_state.worldView)
+      log.debug("HERE")
+      context.become(active(handleSplitBrain.runS(state).unsafeRunSync()))
   }
 
-  private def resetHandleSplitBrain(): Unit = {
-    _handleSplitBrain.cancel()
-    _handleSplitBrain = scheduleHandleSplitBrain()
-  }
+  private def seenChanged(seenBy: Set[Address]): State[StabilityReporterState, Unit] = State.modify(_.flush(seenBy))
 
-  private def scheduleHandleSplitBrain(): Cancellable =
-    context.system.scheduler.scheduleOnce(stableAfter, self, HandleSplitBrain)
+  private def modify(
+    f: StabilityReporterState => StabilityReporterState
+  ): StateT[SyncIO, StabilityReporterState, Unit] =
+    StateT.modifyF { state =>
+      resetHandleSplitBrain.map(_ => f(state))
+    }
+
+  private def memberEvent(e: MemberEvent): StateT[SyncIO, StabilityReporterState, Unit] =
+    modify(_.enqueue(e))
+
+  private def reachableMember(m: Member): StateT[SyncIO, StabilityReporterState, Unit] =
+    modify(_.reachableMember(m))
+
+  private def unreachableMember(m: Member): StateT[SyncIO, StabilityReporterState, Unit] =
+    modify(_.unreachableMember(m))
+
+  private def indirectlyConnected(m: Member): StateT[SyncIO, StabilityReporterState, Unit] =
+    modify(_.indirectlyConnected(m))
+
+  private val handleSplitBrain: StateT[SyncIO, StabilityReporterState, Unit] =
+    StateT.inspectF(state => SyncIO(downer ! Downer.HandleSplitBrain(state.worldView)))
+
+  private val scheduleHandleSplitBrain: SyncIO[Unit] =
+    SyncIO(timers.startSingleTimer(HandleSplitBrain, HandleSplitBrain, stableAfter))
+
+  private val cancelHandleSplitBrain: SyncIO[Unit] = SyncIO(timers.cancel(HandleSplitBrain))
+
+  private val resetHandleSplitBrain: SyncIO[Unit] = cancelHandleSplitBrain >> scheduleHandleSplitBrain
 
   implicit private val ec: ExecutionContext = context.system.dispatcher
 
   override def preStart(): Unit = {
     cluster.subscribe(self, InitialStateAsSnapshot, classOf[akka.cluster.ClusterEvent.MemberEvent])
     context.system.eventStream.subscribe(self, classOf[SeenChanged])
-
   }
 
   override def postStop(): Unit = {
     cluster.unsubscribe(self)
     context.system.eventStream.unsubscribe(self)
-    _handleSplitBrain.cancel()
+    timers.cancel(HandleSplitBrain)
   }
 }
 
