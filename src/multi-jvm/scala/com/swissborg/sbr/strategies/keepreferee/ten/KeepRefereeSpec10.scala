91879b86ff6cabb3bd21aeb8e13e2cc3bab35099
Dennis van der Bij <dennis@swissborg.com>
2019-05-20 14:57:57 +0200

    add keep-referee tests

---
+++ src/multi-jvm/scala/com/swissborg/sbr/strategies/keepreferee/ten/KeepRefereeSpec10.scala
@@ -0,0 +1,53 @@
+package com.swissborg.sbr.strategies.keepreferee.ten
+
+import akka.remote.transport.ThrottlerTransportAdapter.Direction
+import com.swissborg.sbr.TenNodeSpec
+import com.swissborg.sbr.TestUtil.linksToKillForPartitions
+import com.swissborg.sbr.strategies.keepreferee.KeepRefereeSpecTenNodeConfig
+
+import scala.concurrent.duration._
+
+class KeepRefereeSpec10MultiJvmNode1  extends KeepRefereeSpec10
+class KeepRefereeSpec10MultiJvmNode2  extends KeepRefereeSpec10
+class KeepRefereeSpec10MultiJvmNode3  extends KeepRefereeSpec10
+class KeepRefereeSpec10MultiJvmNode4  extends KeepRefereeSpec10
+class KeepRefereeSpec10MultiJvmNode5  extends KeepRefereeSpec10
+class KeepRefereeSpec10MultiJvmNode6  extends KeepRefereeSpec10
+class KeepRefereeSpec10MultiJvmNode7  extends KeepRefereeSpec10
+class KeepRefereeSpec10MultiJvmNode8  extends KeepRefereeSpec10
+class KeepRefereeSpec10MultiJvmNode9  extends KeepRefereeSpec10
+class KeepRefereeSpec10MultiJvmNode10 extends KeepRefereeSpec10
+
+/**
+ * Network partition between node1 -...- node8 and node9 - node10.
+ * Indirect connections between node7 and node8.
+ */
+class KeepRefereeSpec10 extends TenNodeSpec("StaticQuorum", KeepRefereeSpecTenNodeConfig) {
+  override def assertions(): Unit =
+    "handle a mix of a network partition and indirectly connected nodes" in within(120 seconds) {
+      runOn(node1) {
+        linksToKillForPartitions(
+          List(List(node1, node2, node3, node4, node5, node6, node7, node8), List(node9, node10))
+        ).foreach {
+          case (from, to) => testConductor.blackhole(from, to, Direction.Both).await
+        }
+
+        testConductor.blackhole(node7, node8, Direction.Receive).await
+      }
+
+      enterBarrier("links-disconnected")
+
+      runOn(node1, node2, node3, node4, node5, node6) {
+        waitForSurvivors(node1, node2, node3, node4, node5, node6)
+        waitExistsAllDownOrGone(
+          Seq(Seq(node9, node10, node7), Seq(node9, node10, node8))
+        )
+      }
+
+      runOn(node9, node10) {
+        waitForSelfDowning
+      }
+
+      enterBarrier("split-brain-resolved")
+    }
+}
