d3f787a01adfce5304c4b21bb8216d88a4da8d3b
Dennis van der Bij <dennis@swissborg.com>
2019-05-22 15:00:37 +0200

    fix test

--- src/multi-jvm/scala/com/swissborg/sbr/strategies/keepoldest/six/KeepOldestSpec6.scala
+++ src/multi-jvm/scala/com/swissborg/sbr/strategies/keepoldest/six/KeepOldestSpec6.scala
@@ -13,55 +13,49 @@ class KeepOldestSpec6MultiJvmNode4 extends KeepOldestSpec6
 class KeepOldestSpec6MultiJvmNode5 extends KeepOldestSpec6
 
 /**
- * The link between node1 and node4 fails.
+ * The link between node2 and node4 fails.
  * The link between node3 and node5 fails.
  *
- * All nodes but node2 downs itself because they are indirectly connected.
- * Node2 survives as its the oldest node.
+ * All nodes but node1 downs itself because they are indirectly connected.
+ * Node1 survives as its the oldest node.
  */
 class KeepOldestSpec6 extends FiveNodeSpec("KeepOldest", KeepOldestSpecFiveNodeConfig) {
   override def assertions(): Unit =
     "handle indirectly-connected nodes" in within(120 seconds) {
       runOn(node1) {
-        val a = testConductor.blackhole(node1, node4, Direction.Receive).await
+        val a = testConductor.blackhole(node2, node4, Direction.Receive).await
         val b = testConductor.blackhole(node3, node5, Direction.Receive).await
       }
 
       enterBarrier("nodes-disconnected")
 
-      runOn(node1) {
-        waitForUp(node1)
+      runOn(node2) {
+        waitForUp(node2)
         waitToBecomeUnreachable(node4)
       }
 
-      enterBarrier("node4-unreachable")
-
       runOn(node4) {
         waitForUp(node4)
-        waitToBecomeUnreachable(node1)
+        waitToBecomeUnreachable(node2)
       }
-
-      enterBarrier("node1-unreachable")
-
-      ///
 
       runOn(node3) {
         waitForUp(node3)
         waitToBecomeUnreachable(node5)
       }
 
-      enterBarrier("node5-unreachable")
-
       runOn(node5) {
         waitForUp(node5)
         waitToBecomeUnreachable(node3)
       }
 
-      enterBarrier("node3-unreachable")
+      enterBarrier("split-brain")
 
-      ///
+      runOn(node1) {
+        waitForDownOrGone(node2, node3, node4, node5)
+      }
 
-      runOn(node1, node3, node4, node5) {
+      runOn(node2, node3, node4, node5) {
         waitForSelfDowning
       }
 
