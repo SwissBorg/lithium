8f706bf68f98de46f090a867a5162034513f9755
Dennis van der Bij <dennis@swissborg.com>
2019-05-23 14:44:44 +0200

    remove testing helpers duplication

--- src/multi-jvm/scala/com/swissborg/sbr/STMultiNodeSpec.scala
+++
@@ -1,9 +0,0 @@
-package com.swissborg.sbr
-
-import akka.remote.testkit.MultiNodeSpecCallbacks
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
-
-trait STMultiNodeSpec extends MultiNodeSpecCallbacks with WordSpecLike with Matchers with BeforeAndAfterAll {
-  override def beforeAll(): Unit = multiNodeSpecBeforeAll()
-  override def afterAll(): Unit = multiNodeSpecAfterAll()
-}
