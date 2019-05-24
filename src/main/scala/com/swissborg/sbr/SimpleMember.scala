0c27d0e46b1088fdeeb3c0e3d3f64bd90afaf5e4
Dennis van der Bij <dennis@swissborg.com>
2019-05-24 15:16:21 +0200

    fix unstable being triggered during resolution

--- src/main/scala/com/swissborg/sbr/SimpleMember.scala
+++ src/main/scala/com/swissborg/sbr/SimpleMember.scala
@@ -1,6 +1,7 @@
 package com.swissborg.sbr
 
 import akka.cluster.{Member, MemberStatus, UniqueAddress}
+import cats.Eq
 import com.swissborg.sbr.implicits._
 import io.circe.Encoder
 import io.circe.generic.semiauto.deriveEncoder
@@ -11,4 +12,5 @@ object SimpleMember {
   def fromMember(member: Member): SimpleMember = SimpleMember(member.uniqueAddress, member.status)
 
   implicit val simpleMemberEncoder: Encoder[SimpleMember] = deriveEncoder
+  implicit val simpleMemberEq: Eq[SimpleMember]           = Eq.fromUniversalEquals
 }
