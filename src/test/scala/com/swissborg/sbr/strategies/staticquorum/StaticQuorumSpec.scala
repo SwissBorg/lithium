baafc7f908a6d0793a5ee9471a689977a73c05ed
Dennis van der Bij <dennis@swissborg.com>
2019-05-27 14:46:13 +0200

    test up-dissemination on static-quorum

--- src/test/scala/com/swissborg/sbr/strategies/staticquorum/StaticQuorumSpec.scala
+++ src/test/scala/com/swissborg/sbr/strategies/staticquorum/StaticQuorumSpec.scala
@@ -2,10 +2,9 @@ package com.swissborg.sbr.strategies.staticquorum
 
 import cats.effect.SyncIO
 import com.swissborg.sbr._
-import com.swissborg.sbr.scenarios.{OldestRemovedScenario, SymmetricSplitScenario}
+import com.swissborg.sbr.scenarios._
 
 class StaticQuorumSpec extends SBSpec {
-
   "StaticQuorum" must {
     simulate[SyncIO, StaticQuorum, SymmetricSplitScenario](
       "handle symmetric split scenarios with a correctly defined quorum size"
@@ -14,19 +13,7 @@ class StaticQuorumSpec extends SBSpec {
     simulate[SyncIO, StaticQuorum, OldestRemovedScenario]("handle a split during the oldest-removed scenarios")(
       _.unsafeRunSync()
     )
-    // TODO check if can really be handled
-    //    "2 - should handle split during up-dissemination" in {
-    //      forAll { scenario: UpDisseminationScenario =>
-    //        implicit val _: Arbitrary[Config] = com.swissborg.sbr.strategies.staticquorum.StaticQuorumSpec.arbConfig(scenario.clusterSize)
-    //
-    //        forAll { config: Config =>
-    //          val remainingSubClusters = scenario.worldViews.foldMap { worldView =>
-    //            Strategy[StaticQuorum](worldView, config).foldMap(RemainingPartitions.fromDecision)
-    //          }
-    //
-    //          remainingSubClusters.n.value should be <= 1
-    //        }
-    //      }
-    //    }
+
+    simulate[SyncIO, StaticQuorum, UpDisseminationScenario]("handle split during up-dissemination")(_.unsafeRunSync())
   }
 }
