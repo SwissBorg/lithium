6e266e2dd0d7dccf1cc210dcfc2b7061f2154f86
Dennis van der Bij <dennis@swissborg.com>
2019-06-25 20:22:15 +0200

    always use AkkaSystem's config

---
+++ src/multi-jvm/scala/com/swissborg/sbr/strategy/keepoldest/KeepOldestSpec6.scala
@@ -0,0 +1,43 @@
+package com.swissborg.sbr
+package strategy
+package keepoldest
+
+import akka.remote.transport.ThrottlerTransportAdapter.Direction
+import com.swissborg.sbr.TestUtil.linksToKillForPartitions
+
+import scala.concurrent.duration._
+
+class KeepOldestSpec6MultiJvmNode1 extends KeepOldestSpec6
+class KeepOldestSpec6MultiJvmNode2 extends KeepOldestSpec6
+class KeepOldestSpec6MultiJvmNode3 extends KeepOldestSpec6
+class KeepOldestSpec6MultiJvmNode4 extends KeepOldestSpec6
+class KeepOldestSpec6MultiJvmNode5 extends KeepOldestSpec6
+
+/**
+  * The link between node2 and node4 fails.
+  * The link between node3 and node5 fails.
+  *
+  * All nodes but node1 downs itself because they are indirectly connected.
+  * Node1 survives as its the oldest node.
+  */
+class KeepOldestSpec6 extends FiveNodeSpec("KeepOldest", KeepOldestSpecFiveNodeConfig) {
+  override def assertions(): Unit =
+    "handle scenario 6" in within(120 seconds) {
+      runOn(node1) {
+        testConductor.blackhole(node2, node4, Direction.Receive).await
+        testConductor.blackhole(node3, node5, Direction.Receive).await
+      }
+
+      enterBarrier("links-failed")
+
+      runOn(node1) {
+        waitForAllLeaving(node2, node3, node4, node5)
+      }
+
+      runOn(node2, node3, node4, node5) {
+        waitForSelfDowning
+      }
+
+      enterBarrier("split-brain-resolved")
+    }
+}
---
+++ src/multi-jvm/scala/com/swissborg/sbr/strategy/keepreferee/KeepRefereeSpec6.scala
@@ -0,0 +1,42 @@
+package com.swissborg.sbr
+package strategy
+package keepreferee
+
+import akka.remote.transport.ThrottlerTransportAdapter.Direction
+
+import scala.concurrent.duration._
+
+class KeepRefereeSpec6MultiJvmNode1 extends KeepRefereeSpec6
+
+class KeepRefereeSpec6MultiJvmNode2 extends KeepRefereeSpec6
+
+class KeepRefereeSpec6MultiJvmNode3 extends KeepRefereeSpec6
+
+class KeepRefereeSpec6MultiJvmNode4 extends KeepRefereeSpec6
+
+class KeepRefereeSpec6MultiJvmNode5 extends KeepRefereeSpec6
+
+/**
+  * The link between node1 and node4 fails.
+  * The link between node3 and node5 fails.
+  *
+  * All nodes but node2 downs itself because they are indirectly connected.
+  * Node2 survives as it is the referee.
+  */
+class KeepRefereeSpec6 extends FiveNodeSpec("KeepReferee", KeepRefereeSpecFiveNodeConfig) {
+  override def assertions(): Unit =
+    "handle scenario 6" in within(120 seconds) {
+      runOn(node1) {
+        testConductor.blackhole(node1, node4, Direction.Receive).await
+        testConductor.blackhole(node3, node5, Direction.Receive).await
+      }
+
+      enterBarrier("links-failed")
+
+      runOn(node1, node3, node4, node5) {
+        waitForSelfDowning
+      }
+
+      enterBarrier("split-brain-resolved")
+    }
+}
