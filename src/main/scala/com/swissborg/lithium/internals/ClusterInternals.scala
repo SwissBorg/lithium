c6b80a33c886981a9f8c3dba348d02545cbb99d9
Dennis van der Bij <dennis@swissborg.com>
2019-08-08 20:31:37 +0200

    better IC detection

---
+++ src/main/scala/com/swissborg/lithium/internals/ClusterInternals.scala
@@ -0,0 +1,18 @@
+package com.swissborg.lithium
+
+package internals
+
+import akka.actor._
+import akka.cluster.swissborg.ClusterInternalsPublisher
+
+/**
+  * @see [[ClusterInternalsImpl]]
+  */
+private[lithium] object ClusterInternals extends ExtensionId[ClusterInternalsImpl] with ExtensionIdProvider {
+  override def createExtension(system: ExtendedActorSystem): ClusterInternalsImpl = {
+    discard(system.actorOf(ClusterInternalsPublisher.props))
+    new ClusterInternalsImpl(system)
+  }
+
+  override def lookup(): ExtensionId[_ <: Extension] = ClusterInternals
+}
---
+++ src/main/scala/com/swissborg/lithium/internals/LithiumSeenChanged.scala
@@ -0,0 +1,11 @@
+package com.swissborg.lithium
+
+package internals
+
+import akka.actor.Address
+
+/**
+  * Mirror of [[akka.cluster.ClusterEvent.SeenChanged]]
+  * so it can be subscribed to by actors in other packages.
+  */
+final case class LithiumSeenChanged(convergence: Boolean, seenBy: Set[Address])
